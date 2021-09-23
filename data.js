390327a8c8165203b2d4ec78e7bd99c1dd5bb57c
AleMonteagudo <91240597+AleMonteagudo@users.noreply.github.com>
2021-09-23 00:02:24 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,201 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-iglesia-el-pilar",
+      "name": "IGLESIA EL PILAR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.277271951283735,
+        "pitch": -0.4051730930705695,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.116392802326068,
+          "pitch": 0.16374667089397654,
+          "rotation": 0,
+          "target": "1-torre-vicentina"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.9115502273446943,
+          "pitch": 0.06317294420707142,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">GANCHO</font></font>",
+          "text": "<span style=\"font-size: 11pt; line-height: 107%; font-family: Calibri, sans-serif;\">Los orígenes de este santuario se debe a un\nacontecimiento peculiar que ahora que se ha convertido en toda un anécdota.&nbsp;</span><br>"
+        },
+        {
+          "yaw": -2.2715198513861115,
+          "pitch": -0.23950693281601332,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">DESARROLLO</font></font>",
+          "text": "<span style=\"font-size: 11pt; line-height: 107%; font-family: Calibri, sans-serif;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Inicio la construcción en 1762 pero este falleció y no pudo ver la obra terminada a esto quedo su esposa y esta le encargo a don Francisco Quintanilla la construcción pero este falleció dos años después y finalmente la hija de los esposos estuvo a cargo hasta concretarla en 1769.</font></font></span><br>"
+        },
+        {
+          "yaw": -0.8829830042960438,
+          "pitch": -0.3150573333058304,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">JEFE INDIGENA</font></font>",
+          "text": "<span style=\"font-size: 11pt; line-height: 107%; font-family: Calibri, sans-serif;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Anastasio Aquino se coronó como el “Emperador de los Nonualcos” Tiempo después Anastasio fue apresado y ejecutado por sus actos.</font></font></span><br>"
+        },
+        {
+          "yaw": 0.5534848792735474,
+          "pitch": 0.08729399081586209,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">CONCLUSIÓN</font></font>",
+          "text": "<span style=\"font-size:11.0pt;line-height:107%;\nfont-family:&quot;Calibri&quot;,&quot;sans-serif&quot;;mso-ascii-theme-font:minor-latin;mso-fareast-font-family:\nCalibri;mso-fareast-theme-font:minor-latin;mso-hansi-theme-font:minor-latin;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-bidi-theme-font:minor-bidi;\nmso-ansi-language:ES-SV;mso-fareast-language:EN-US;mso-bidi-language:AR-SA\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Si usted desea visitarla puede hacerlo y de paso puede descansar en la plaza incluso comer cerca ya que esta se encuentra en el centro de san vicente .</font></font></span><br>"
+        }
+      ]
+    },
+    {
+      "id": "1-torre-vicentina",
+      "name": "TORRE VICENTINA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.24705713514281058,
+        "pitch": 0.17433034812363601,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4766064598300055,
+          "pitch": 0.3283720679612614,
+          "rotation": 0,
+          "target": "2-laguna-de-apastepeque"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5324366732776937,
+          "pitch": -0.3203598553316702,
+          "title": "<span style=\"font-size: 13px; background-color: rgba(58, 68, 84, 0.8);\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">DSARROLLO</font></font></span>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">&nbsp;El 1 de febrero de 1928 empezó y finalizó en 1930.</font></font></font></font>"
+        },
+        {
+          "yaw": 2.858844385776436,
+          "pitch": -0.0832173964155345,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">TERREMOTOS</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">La Torre soporto varios sismos y terremotos pero no conto con la misma suerte con los del 2001 desde ese momento quedo inservibe.</font></font></font></font>"
+        },
+        {
+          "yaw": 1.9950183816924447,
+          "pitch": 0.6521129474711618,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">&nbsp;NUEVA TORRE</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">&nbsp;En septiembre del 2007 el gobierno a través de CONCULTURA encargo a la FUNDACION SAN VICENTE elaborar un proyecto para la nueva torre, y fue asi que desde el 2007 hasta el 2009 se construyó la nueva TORRE VICENTINA.</font></font>"
+        },
+        {
+          "yaw": 0.15472051217972904,
+          "pitch": -0.29023866872974935,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">&nbsp;GANCHO</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">&nbsp;PARQUE ANTONIO JOSE CAÑAS en el cual esta ubicado su famosa TORRE VICENTINA de igual manera a un costado su catedral y el antiguo palacio municipal.</font></font>"
+        },
+        {
+          "yaw": 1.2271410013896666,
+          "pitch": -0.1792731344906393,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">CONCLUSIÓN</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Sin duda la TORRE VICENTINA tan elegante y blanca es todo un importante atractivo que tenemos que aprender a valorar.</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "2-laguna-de-apastepeque",
+      "name": "LAGUNA DE APASTEPEQUE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.590719294328526,
+        "pitch": -0.019401794803400207,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.383615024057642,
+          "pitch": -0.11314821631352778,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">GANCHO</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Antiguamente era un cerro llamado CERRO DE APASTE y la gente mayor comentaba que a raiz de un temblor este desaparecio y se hizo la LAGUNA.</font></font>"
+        },
+        {
+          "yaw": 2.6580959629430723,
+          "pitch": 0.2285109948277544,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">DESAROLLO</font></font>",
+          "text": "<span style=\"font-size: 12pt; line-height: 107%; font-family: &quot;Times New Roman&quot;, serif;\">En\nel año de 1963 se decretó el embellecimiento de los alrededores de la Laguna de\nApastepeque, considerándose de utilidad pública, siendo inaugurado el\nturicentro en el año de 1979 por el Lic. Alfredo Magaña.&nbsp;</span><br>"
+        },
+        {
+          "yaw": -2.6316074546117036,
+          "pitch": -0.1200431691856032,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">ADMINISTRACIÓN</font></font>",
+          "text": "<span style=\"font-size: 12pt; line-height: 107%; font-family: &quot;Times New Roman&quot;, serif;\">Instituto\nSalvadoreño de Turismo (ISTU).</span><br>"
+        },
+        {
+          "yaw": -1.4110272011791238,
+          "pitch": -0.13717643857424022,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">PRECIO Y HORARIO</font></font>",
+          "text": "<span style=\"font-size: 12pt; line-height: 107%; font-family: &quot;Times New Roman&quot;, serif;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">La Entrada para visitantes nacionales: $ 1.50 Entrada a visitantes extranjeros $ 3.00.</font></font></font></font></span><div><span style=\"font-size: 12pt; line-height: 107%; font-family: &quot;Times New Roman&quot;, serif;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><br></font></font></span></div><div><span style=\"font-size: 12pt; line-height: 107%; font-family: &quot;Times New Roman&quot;, serif;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">&nbsp;Los Horario son de:</font></font></span></div><div><span style=\"font-size: 12pt; line-height: 107%; font-family: &quot;Times New Roman&quot;, serif;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">&nbsp;8:00 am a 4:00 pm&nbsp;</font></font></span><br></div>"
+        },
+        {
+          "yaw": 0.12876794327418395,
+          "pitch": 0.3447496351324695,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">CONCLUSIÓN</font></font>",
+          "text": "<span style=\"font-size: 12pt; line-height: 107%; font-family: &quot;Times New Roman&quot;, serif;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Sin duda la laguna es un lugar impresionante para recorrer y tener un día relajante actualmente hay más oferta turística en este lugar por lo cual está siendo muy conocido por la población salvadoreña.</font></font></span><br>"
+        }
+      ]
+    }
+  ],
+  "name": "EVALUACION",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
